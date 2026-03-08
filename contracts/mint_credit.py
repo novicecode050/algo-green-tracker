a742da746397e8201ff5a5aee504553aacdc4bed
M. LAKSHMI TANOOJA <tanoojaml@gmail.com>
2026-03-08 20:44:01 +0530

    feat: add initial smart contract skeleton for carbon credit minting
    Add a basic Algorand smart contract for carbon credit minting.

---
+++ contracts/mint_credit.py
@@ -0,0 +1,8 @@
+# Basic Algorand Smart Contract Placeholder for Carbon Credit Minting
+from pyteal import *
+
+def approval_program():
+    return Approve()
+
+def clear_state_program():
+    return Approve()
